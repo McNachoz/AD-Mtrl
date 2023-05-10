2aee00fabd849318a8b443c6c3504787a78f25ec
McNachoz <website.unb@example.com>
2023-05-10 11:04:56 -0300

    16th commiit

--- Web/custom.js/aos.js
+++ Web/custom.js/aos.js
@@ -1,1 +1,27 @@
-AOS.init();
+// AOS.init();
+
+// $(document).ready(function() {
+
+//     AOS.init();
+
+//     $("#fullview").fullView({
+//       //Navigation
+//       navbar: "#navbar",
+//       dots: true,
+//       dotsPosition: 'right',
+//       //Scrolling
+//       easing: 'swing',
+//       // backToTop: true,
+//       // Accessibility
+//       keyboardScrolling: true,
+//       // Callback
+//       onViewChange: function(currentView) {
+//          // console.log(currentView)
+//       }
+//     })
+
+//  });
+
+$(function() {
+    AOS.init();
+  });
