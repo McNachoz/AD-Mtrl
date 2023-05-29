83e619c540916456cc5feadb3a2e516115f1e34e
McNachoz <website.unb@example.com>
2023-05-29 20:32:39 -0300

    17th commit

--- Web/custom.js/navbar_collapse.js
+++ Web/custom.js/navbar_collapse.js
@@ -28,41 +28,7 @@ $('.back-to-top').click(function () {
 $("#header").sticky({topSpacing:0, zIndex: '50'});
 
 
-// document.body.style.zoom="80%"
 
-  // Smooth scroll for the menu and links with .scrollto classes
-  // $('.nav-menu a, #mobile-nav a, .scrollto').on('click', function() {
-  //   if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-  //     var target = $(this.hash);
-  //     if (target.length) {
-  //       var top_space = 0;
-
-  //       if ($('#header').length) {
-  //         top_space = $('#header').outerHeight();
-
-  //         if( ! $('#header').hasClass('header-fixed') ) {
-  //           top_space = top_space - 20;
-  //         }
-  //       }
-
-  //       $('html, body').animate({
-  //         scrollTop: target.offset().top - top_space
-  //       }, 500, 'easeInOutExpo');
-
-  //       if ($(this).parents('.nav-menu').length) {
-  //         $('.nav-menu .menu-active').removeClass('menu-active');
-  //         $(this).closest('li').addClass('menu-active');
-  //       }
-
-  //       if ($('body').hasClass('mobile-nav-active')) {
-  //         $('body').removeClass('mobile-nav-active');
-  //         $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
-  //         $('#mobile-body-overly').fadeOut();
-  //       }
-  //       return false;
-  //     }
-  //   }
-  // });
 
 function isElementInViewport (el) {
   if (typeof jQuery === "function" && el instanceof jQuery) {
@@ -84,29 +50,13 @@ $("[data-aos]").each(function() {
 });
 
 
-// $("#myModal").modal("show");
-// $("#myModal").css("z-index", "1500");
 
-
-// var element = document.getElementById("body");
-// element.innerHTML += "<iframe src='http://www.google.com' height='200' width='" + document.body.clientWidth * 0.8 + "'/>";
-
-// window.onload = function(){
-//   let ifElem = document.getElementById("iframe-id");
-//   ifElem.contentWindow.document.documentElement.style.fontSize="80%";
-// }
 
 });
 
 
 
-// jQuery(document).ready(function( $ ) {
 
-//   // Stick the header at top on scroll
-//   $("#header").sticky({topSpacing:0, zIndex: '50'});
-
-
-// });
 
 
 
--- Web/custom.js/script.js
+++ Web/custom.js/script.js
@@ -28,44 +28,9 @@
         });
     }
 
-    // window.setTimeout(function () {
-    //     var height = document.body.scrollHeight.toString();
-    //     window.external.notify("ContentHeight:" + height);
-    // }, 100);
-
 
 })(jQuery);
 
 
-// smooth scroll
-// $(function(){
-
-// }
-// )
-// if($(".modal").length>0) {
-//     $(".modal").each(function() {
-//         $(".modal").prependTo( "body" );
-//     });
-// }
-
-// const onChange = e => {
-//     const pr = window.devicePixelRatio;
-//     const media = `(resolution: ${pr}dppx)`;
-//     const mql = matchMedia(media);
-
-//     const prString = (pr * 100).toFixed(0);
-//     const textContent = `${prString}% (${pr.toFixed(2)})`;
-
-//     console.log(textContent);
-//     document.getElementById('out').append(
-//       Object.assign(document.createElement('li'), {textContent})
-//     )
-
-//     mql.addEventListener('change', onChange, {once: true});
-//   };
-
-//   document.getElementById('checkZoom').addEventListener('click', e => onChange());
-
-//   onChange();
 
 
