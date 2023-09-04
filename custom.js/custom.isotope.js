c1a8a5c34ee1f0778a0c910bc395e83f47650f71
McChikenz <106206847+McChikenz@users.noreply.github.com>
2023-09-04 21:03:53 +0000

    Deploying to gh-pages from  @ 59c12e9800951008fa2f07eecc60b440b454c3cd 🚀

---
+++ custom.js/custom.isotope.js
@@ -0,0 +1,45 @@
+$(window).load(function(){
+
+    var teamIsotope = $('.team-container').isotope({
+        itemSelector: 'team-thumbnail',
+        layoutMode: 'fitRows'
+    });
+
+$('#team-filters li').on('click', function(){
+    $("team-filters li").removeClass('filter-active');
+    $(this).addClass('filter-active');
+
+    teamIsotope.isotope({ filter: $(this).data('filter')});
+})
+})
+
+
+// debounce so filtering doesn't happen every millisecond
+function debounce( fn, threshold ) {
+    var timeout;
+    threshold = threshold || 100;
+    return function debounced() {
+        clearTimeout( timeout );
+        var args = arguments;
+        var _this = this;
+        function delayed() {
+            fn.apply( _this, args );
+        }
+        timeout = setTimeout( delayed, threshold );
+    };
+}
+
+
+
+ // trigger isotope again after images have loaded
+ $container.imagesLoaded( function(){
+   $(window).smartresize();
+ });
+
+$(window).resize(function(){
+    $("#isotope").masonry().masonry("reloadItems");
+ });
+
+
+
+
