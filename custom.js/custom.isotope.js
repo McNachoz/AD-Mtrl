3aef1d38b56d2d7929d5b7a490e27cbfce643ef5
McNachoz <126591069+McNachoz@users.noreply.github.com>
2023-09-05 23:31:21 +0000

    Deploying to gh-pages from  @ c412e5c538ce223d083c54f2844eba475baa8386 🚀

--- custom.js/custom.isotope.js
+++ custom.js/custom.isotope.js
@@ -15,19 +15,19 @@
 
 
 // debounce so filtering doesn't happen every millisecond
-function debounce( fn, threshold ) {
-    var timeout;
-    threshold = threshold || 100;
-    return function debounced() {
-        clearTimeout( timeout );
-        var args = arguments;
-        var _this = this;
-        function delayed() {
-            fn.apply( _this, args );
-        }
-        timeout = setTimeout( delayed, threshold );
-    };
-}
+// function debounce( fn, threshold ) {
+//     var timeout;
+//     threshold = threshold || 100;
+//     return function debounced() {
+//         clearTimeout( timeout );
+//         var args = arguments;
+//         var _this = this;
+//         function delayed() {
+//             fn.apply( _this, args );
+//         }
+//         timeout = setTimeout( delayed, threshold );
+//     };
+// }
 
 
 
