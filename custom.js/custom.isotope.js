3f0672c1e1c5be0280e36546eee8611c003db574
McChikenz <website.unb@gmail.com>
2023-09-05 21:50:35 -0400

    commit

--- custom.js/custom.isotope.js
+++ custom.js/custom.isotope.js
@@ -1,5 +1,10 @@
 $(window).load(function(){
 
+    var teamIsotope = $('.team-container').isotope({
+        itemSelector: 'team-thumbnail',
+        layoutMode: 'fitRows'
+    });
+
 $('#team-filters li').on('click', function(){
     $("team-filters li").removeClass('filter-active');
     $(this).addClass('filter-active');
