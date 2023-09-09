8fae8b03f64548e8d5c69c7ecb7886fcd6bd7c9b
McNachoz <126591069+McNachoz@users.noreply.github.com>
2023-09-09 01:31:45 +0000

    Deploying to gh-pages from  @ e02ba2b39e5b074d47754941a351c4292b2e20fd 🚀

--- custom.js/custom.isotope.js
+++ custom.js/custom.isotope.js
@@ -1,17 +1,17 @@
-// $(window).load(function(){
+$(window).load(function(){
 
-//     var teamIsotope = $('.team-container').isotope({
-//         itemSelector: 'team-thumbnail',
-//         layoutMode: 'fitRows'
-//     });
+    var teamIsotope = $('.team-container').isotope({
+        itemSelector: 'team-thumbnail',
+        layoutMode: 'fitRows'
+    });
 
-// $('#team-filters li').on('click', function(){
-//     $("team-filters li").removeClass('filter-active');
-//     $(this).addClass('filter-active');
+$('#team-filters li').on('click', function(){
+    $("team-filters li").removeClass('filter-active');
+    $(this).addClass('filter-active');
 
-//     teamIsotope.isotope({ filter: $(this).data('filter')});
-// })
-// })
+    teamIsotope.isotope({ filter: $(this).data('filter')});
+})
+})
 
 
 
