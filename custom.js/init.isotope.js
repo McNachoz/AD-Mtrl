27d9a22d227eb2bf7d43591491423bf215499bb5
McNachoz <126591069+McNachoz@users.noreply.github.com>
2023-09-22 01:26:11 +0000

    Deploying to gh-pages from  @ 636766a86f3dfbc30872d08b9b7a379b847124c4 🚀

--- custom.js/init.isotope.js
+++ custom.js/init.isotope.js
@@ -3,11 +3,8 @@ var $grid = $('#team-area').isotope({
     // options
   });
   // filter items on button click
-
-
   $('.filter-button-group').on( 'click', 'button', function() {
     var filterValue = $(this).attr('data-filter');
     $grid.isotope({ filter: filterValue });
   });
-
 
